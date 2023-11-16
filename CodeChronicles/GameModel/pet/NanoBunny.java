37f6111b98aa20d3856fd596356476ab1555f8a4
Hia Aggrawal <hia.aggrawal@mail.utoronto.ca>
2023-11-16 12:36:10 -0500

    hia

---
+++ CodeChronicles/GameModel/pet/NanoBunny.java
@@ -0,0 +1,30 @@
+package GameModel.pet;
+
+import GameModel.Pet;
+
+import java.util.ArrayList;
+import java.util.concurrent.Callable;
+import java.util.HashMap;
+import java.util.List;
+
+/**
+ * This class keeps track of the progress
+ * of the player in the game.
+ */
+public class NanoBunny extends Pet {
+    /**
+     * Adventure Game Player Constructor
+     */
+    public NanoBunny(String name) {
+        super(name);
+        this.introduction = "";
+        this.commands = new HashMap<>();
+    }
+
+    /**
+     * This method takes command from the player for the pet.
+     *
+     * @param command the command given to this pet.
+     */
+    public void giveCommand(String command) {;}
+}
