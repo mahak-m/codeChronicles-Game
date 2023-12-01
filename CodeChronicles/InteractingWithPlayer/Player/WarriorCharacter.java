fac9d8966f767739771440cb33e088b1f9662173
Mahak Mishra <mahak.mishra@mail.utoronto.ca>
2023-12-01 23:21:41 +0000

    Update WarriorCharacter.java so that the inventory methods are removed

--- CodeChronicles/InteractingWithPlayer/Player/WarriorCharacter.java
+++ CodeChronicles/InteractingWithPlayer/Player/WarriorCharacter.java
@@ -31,7 +31,6 @@ public class WarriorCharacter extends Player {
     public Image characterImage;
     private AdventureObject diamondShield;
     public boolean shieldUsed;
-    private ArrayList<AdventureObject> inventory;
     public String playerType;
 
     /**
@@ -47,8 +46,6 @@ public class WarriorCharacter extends Player {
         this.shieldUsed = false;
         // load the image
         characterImage = new Image("OtherFiles/characterImages/warriorCharacter.png");
-        // add special objects to inventory
-        inventory.add(diamondShield);
         // change playerType
         playerType = "Warrior";
     }
@@ -61,72 +58,5 @@ public class WarriorCharacter extends Player {
     }
     // IMPORTANT: to use the image in UI (i.e. in pane, panel):
     // ImageView mageImageView = mageCharacter.getCharacterImageView();
-
-    /**
-     * equiptItem
-     * ________________________
-     * This method picks up and adds an object to a characters inventory, removing it from the room.
-     * if the object is present in the room.  It then returns true.
-     * If the object is not present in the room, the method
-     * returns false.
-     *
-     * @param object (name of the magical object to take)
-     * @return true/false (depending on if the object was or was not taken)
-     */
-    public boolean equiptItem(String object) {
-        if (!currLocation.checkIfObjectInRoom(object)) {
-            return false;
-        }
-        for (AdventureObject obj : currLocation.objectsInRoom) {
-            if (obj.getName().equalsIgnoreCase(object)) {
-                inventory.add(obj); // add object to the players inventory
-                // currLocation(obj); // remove object from their room
-                return true;
-            }
-        }
-        // otherwise, return false if the object was NOT found
-        return false;
-    }
-
-    /**
-     * unEquiptItem
-     * _______________________
-     * This method "drops" an object, removing it from the character's inventory
-     * as a result. (This only happens if the object exists AND is currently
-     * in the players inventory. If either of those conditions are not met,
-     * this method should do nothing.)
-     * The object, once dropped, should be added to the current room.
-     * <p>
-     * * @param s String name of prop or object to be removed to the inventory.
-     */
-    public void unEquiptItem(String s) {
-        // System.out.println("inventory size: " + inventory.size());
-        if (!inventory.isEmpty()) {
-            for (AdventureObject k : inventory) {
-                if (Objects.equals(k.getName(), s)) {
-                    currLocation.objectsInRoom.add(k);
-                    inventory.remove(k);
-                    break; // item removed so break
-                }
-            }
-        }
-    }
-
-    /**
-     * getInventory
-     * _________________________
-     * Getter method for the character's inventory.
-     *
-     * @return current character's inventory.
-     */
-    public String[] getInventory() {
-        return inventory.toArray(new String[inventory.size()]);
-    }
-
-    public void useShield() {
-        // add this in later
-    }
-
-    // override castSpell
 
 }
